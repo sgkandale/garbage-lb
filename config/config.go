7dd4de20dfcd8bccd67af4796b1c5d2fc66f2b5a
Shantanu Kandale <sgkandale@gmail.com>
2022-01-23 18:17:19 +0530

    added json to config

--- config/config.go
+++ config/config.go
@@ -1,62 +1,62 @@
 package config
 
 type Listener struct {
-	Name       string
-	Port       int
-	TLS        bool
-	CertPath   string
-	KeyPath    string
-	DomainName string
-	Type       string
-	Listening  bool
-	Filter     *Filter
+	Name       string  `json:"name,omitempty"`
+	Port       int     `json:"port,omitempty"`
+	TLS        bool    `json:"tls,omitempty"`
+	CertPath   string  `json:"certPath,omitempty"`
+	KeyPath    string  `json:"keyPath,omitempty"`
+	DomainName string  `json:"domainName,omitempty"`
+	Type       string  `json:"type,omitempty"`
+	Listening  bool    `json:"listening,omitempty"`
+	Filter     *Filter `json:"filter,omitempty"`
 }
 
 type Endpoint struct {
-	Name    string
-	Address string
-	Port    int
-	Healthy bool
+	Name    string `json:"name,omitempty"`
+	Address string `json:"address,omitempty"`
+	Port    int    `json:"port,omitempty"`
+	Healthy bool   `json:"healthy,omitempty"`
 }
 
 type ClusterHealth struct {
-	Status         string
-	HealthyCount   int
-	UnhealthyCount int
-	DegradedCount  int
+	Status         string `json:"status,omitempty"`
+	HealthyCount   int    `json:"healthyCount,omitempty"`
+	UnhealthyCount int    `json:"unhealthyCount,omitempty"`
+	DegradedCount  int    `json:"degradedCount,omitempty"`
 }
 
 type Cluster struct {
-	Name string
-	// Type      string
-	Policy    string
-	Endpoints []*Endpoint
-	Health    *ClusterHealth
+	Name string `json:"name,omitempty"`
+	// Type      string `json:"type,omitempty"`
+	Policy    string         `json:"policy,omitempty"`
+	Endpoints []*Endpoint    `json:"endpoints,omitempty"`
+	Health    *ClusterHealth `json:"health,omitempty"`
 }
 
 type Rule struct {
-	Name          string
-	Type          string
-	Value         string
-	Subvalue      string
-	Action        string
-	Enabled       bool
-	Cluster       string
-	TargetCluster *Cluster
+	Name          string   `json:"name,omitempty"`
+	Type          string   `json:"type,omitempty"`
+	Value         string   `json:"value,omitempty"`
+	Subvalue      string   `json:"subvalue,omitempty"`
+	Action        string   `json:"action,omitempty"`
+	Enabled       bool     `json:"enabled,omitempty"`
+	Cluster       string   `json:"cluster,omitempty"`
+	TargetCluster *Cluster `json:"targetCluster,omitempty"`
 }
 
 type Filter struct {
-	Name  string
-	Rules []*Rule
+	Name  string  `json:"name,omitempty"`
+	Rules []*Rule `json:"rules,omitempty"`
 }
 
 type Admin struct {
-	Port    int
-	Enabled bool
+	Port    int  `json:"port,omitempty"`
+	Enabled bool `json:"enabled,omitempty"`
 }
 
 type ConfigStruct struct {
-	Admin     *Admin
-	Listeners []*Listener
-	Clusters  []*Cluster
+	Admin     *Admin      `json:"admin,omitempty"`
+	Listeners []*Listener `json:"listeners,omitempty"`
+	Clusters  []*Cluster  `json:"clusters,omitempty"`
 }
