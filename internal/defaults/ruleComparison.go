cd36e5ccb0111573d56e4d25858aeccf0c4f2613
Shantanu Kandale <sgkandale@gmail.com>
2022-02-12 19:14:37 +0530

    rule comparisons

---
+++ internal/defaults/ruleComparison.go
@@ -0,0 +1,51 @@
+package defaults
+
+import "strings"
+
+var RuleComparisons = []string{
+	"equals",
+	"not_equals",
+	"contains",
+	"not_contains",
+	"starts_with",
+	"ends_with",
+	"matches",
+	"not_matches",
+	"greater_than",
+	"less_than",
+	"greater_than_or_equal",
+	"less_than_or_equal",
+	"not_required",
+}
+
+func IsComparisonValid(givenComparison, givenValue, incomingValue string) bool {
+	switch givenComparison {
+	case "equals":
+		return strings.EqualFold(givenValue, incomingValue)
+	case "not_equals":
+		return !strings.EqualFold(givenValue, incomingValue)
+	case "contains":
+		return strings.Contains(incomingValue, givenValue)
+	case "not_contains":
+		return !strings.Contains(incomingValue, givenValue)
+	case "starts_with":
+		return strings.HasPrefix(incomingValue, givenValue)
+	case "ends_with":
+		return strings.HasSuffix(incomingValue, givenValue)
+	case "matches":
+		return strings.EqualFold(incomingValue, givenValue)
+	case "not_matches":
+		return !strings.EqualFold(incomingValue, givenValue)
+	case "greater_than":
+		return incomingValue > givenValue
+	case "less_than":
+		return incomingValue < givenValue
+	case "greater_than_or_equal":
+		return incomingValue >= givenValue
+	case "less_than_or_equal":
+		return incomingValue <= givenValue
+	case "not_required":
+		return true
+	}
+	return false
+}
