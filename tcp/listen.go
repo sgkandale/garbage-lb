e9494afbc1dfd2987e1eae8c110ec9dff0bc3844
Shantanu Kandale <sgkandale@gmail.com>
2022-02-13 10:45:52 +0530

    fixed tcp listener blocking the thread

--- tcp/listen.go
+++ tcp/listen.go
@@ -58,37 +58,40 @@ func (server *TCPServer) Listen(wg *sync.WaitGroup, listener *config.Listener) {
 		}
 	}(server.Listener, loopCtx)
 
-connectionLoop:
-	for {
-		if server.TerminateSignalled {
-			log.Printf(
-				"%s of type {%s} shutting down...",
-				listener.Name,
-				listener.Type,
-			)
-			cancelLoopCtx()
-			wg.Done()
-			break connectionLoop
-		} else {
-			// wait for a connection
-			conn, err := server.Server.Accept()
-			if err != nil {
+	// accept connections in seperate goroutine
+	go func(listener *config.Listener, ctx context.Context) {
+	connectionLoop:
+		for {
+			if server.TerminateSignalled {
 				log.Printf(
-					"error accepting connection in listener {%s} : %s",
+					"%s of type {%s} shutting down...",
 					listener.Name,
-					err.Error(),
+					listener.Type,
 				)
-				// continue with loop
-				continue connectionLoop
-			}
+				cancelLoopCtx()
+				wg.Done()
+				break connectionLoop
+			} else {
+				// wait for a connection
+				conn, err := server.Server.Accept()
+				if err != nil {
+					log.Printf(
+						"error accepting connection in listener {%s} : %s",
+						listener.Name,
+						err.Error(),
+					)
+					// continue with loop
+					continue connectionLoop
+				}
 
-			// handle the connection in a new goroutine
-			go func(newConn net.Conn) {
-				// lb handler
-				server.LBHandler(newConn)
-				// newConn.Close()
-			}(conn)
+				// handle the connection in a new goroutine
+				go func(newConn net.Conn) {
+					// lb handler
+					server.LBHandler(newConn)
+					// newConn.Close()
+				}(conn)
+			}
 		}
-	}
+	}(server.Listener, loopCtx)
 
 }
