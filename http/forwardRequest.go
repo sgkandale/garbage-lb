c3315199604ecc4b6a4d4339c6cd2348b1a1bf4b
Shantanu Kandale <sgkandale@gmail.com>
2022-02-19 22:40:56 +0530

    request modification

--- http/forwardRequest.go
+++ http/forwardRequest.go
@@ -1,10 +1,10 @@
 package http
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	goHttp "net/http"
-	"net/http/httputil"
-	"net/url"
 
 	"garbagelb/config"
 )
@@ -14,15 +14,65 @@ func forwardRequest(w *goHttp.ResponseWriter, r *goHttp.Request, endpoint *confi
 	// increment active connections counter
 	endpoint.IncrementActiveConnectionsCounter()
 
-	// forward request
-	reverseProxy := httputil.NewSingleHostReverseProxy(
-		&url.URL{
-			Host:   fmt.Sprintf("%s:%d", endpoint.Address, endpoint.Port),
-			Scheme: "http",
-		},
-	)
-	reverseProxy.ServeHTTP(*w, r)
+	// decrement active connections counter on exit
+	defer endpoint.DecrementActiveConnectionsCounter()
 
-	// decrement active connections counter
-	endpoint.DecrementActiveConnectionsCounter()
+	// construct url from endpoint
+	endpointURL := fmt.Sprintf(
+		"%s://%s:%d%s",
+		endpoint.Protocol,
+		endpoint.Address,
+		endpoint.Port,
+		r.URL.Path,
+	)
+
+	// copy body for new request
+	toForwardBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		goHttp.Error(*w, err.Error(), goHttp.StatusInternalServerError)
+		return
+	}
+
+	// request to forward
+	endpointRequest, err := goHttp.NewRequest(
+		r.Method,
+		endpointURL,
+		bytes.NewReader(toForwardBody),
+	)
+	if err != nil {
+		goHttp.Error(*w, err.Error(), goHttp.StatusBadGateway)
+		return
+	}
+	for header, values := range r.Header {
+		for _, value := range values {
+			endpointRequest.Header.Add(header, value)
+		}
+	}
+
+	httpClient := goHttp.Client{}
+
+	responseFromEndpoint, err := httpClient.Do(endpointRequest)
+	if err != nil {
+		goHttp.Error(*w, err.Error(), goHttp.StatusBadGateway)
+		return
+	}
+
+	// copy headers from response
+	for key, value := range responseFromEndpoint.Header {
+		for _, eachValue := range value {
+			(*w).Header().Add(key, eachValue)
+		}
+	}
+
+	// copy body to http writer
+	receivedBody, err := ioutil.ReadAll(responseFromEndpoint.Body)
+	if err != nil {
+		goHttp.Error(*w, err.Error(), goHttp.StatusInternalServerError)
+		return
+	}
+
+	(*w).Write(receivedBody)
+
+	responseFromEndpoint.Body.Close()
+
 }
