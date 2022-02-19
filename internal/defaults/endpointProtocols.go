f1de4982185e595ec22920f8838243c892b47aae
Shantanu Kandale <sgkandale@gmail.com>
2022-02-19 22:38:32 +0530

    revised endpoint protocols

--- internal/defaults/endpointProtocols.go
+++ internal/defaults/endpointProtocols.go
@@ -7,11 +7,17 @@ func GetEndpointProtocol(protocol string) string {
 	if strings.EqualFold(protocol, "http") {
 		return "http"
 	}
+	if strings.EqualFold(protocol, "https") {
+		return "https"
+	}
 	if strings.EqualFold(protocol, "tcp") {
 		return "tcp"
 	}
-	if strings.EqualFold(protocol, "udp") {
-		return "udp"
+	if strings.EqualFold(protocol, "tcp4") {
+		return "tcp4"
+	}
+	if strings.EqualFold(protocol, "tcp6") {
+		return "tcp6"
 	}
 
 	return ""
